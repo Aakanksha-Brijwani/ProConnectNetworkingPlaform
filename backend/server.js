86701091389d00990581e871f88d8524abbfd3e7
Aakanksha Brijwani <aakankshabrijwani@gmail.com>
2026-07-10 12:37:26 +0530

    Fix Render deployment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+
 import postRouters from "./routes/posts.routes.js";
 import userRouters from "./routes/user.routes.js";
 
@@ -11,19 +13,27 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(postRouters);
 app.use(userRouters);
-app.use(express.static("uploads"));
-app.use(express.json());
 
+app.use(express.static("uploads"));
+
+const PORT = process.env.PORT || 5001;
 
 const start = async () => {
-  const connectDB = await mongoose.connect(
-    "mongodb://aakankshabrijwani_db_user:Z3ibSAegr9UdYzM2@ac-mtx7sam-shard-00-00.ajjcnze.mongodb.net:27017,ac-mtx7sam-shard-00-01.ajjcnze.mongodb.net:27017,ac-mtx7sam-shard-00-02.ajjcnze.mongodb.net:27017/?ssl=true&replicaSet=atlas-vjczdu-shard-0&authSource=admin&appName=apnaproconnect",
-  );
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
 
-  app.listen(5001, () => {
-    console.log("Server is running on port 5001");
-  });
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
 };
+
 start();
